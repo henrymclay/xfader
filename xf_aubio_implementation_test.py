7c89f4ecbfafa56822a876809db4bf2f5c8b64bc
Henry Clay <mr.henry.clay@gmail.com>
2025-02-23 11:24:27 -0500

    aubio start

--- xf_aubio_implementation_test.py
+++ xf_aubio_implementation_test.py
@@ -10,6 +10,7 @@ import pytest
 import xfader 
 import numpy
 import aubio
+import os
 from pydub import AudioSegment 
 from pydub.playback import play
 
@@ -17,5 +18,20 @@ from pydub.playback import play
 
 def test_aubio():
     return
+
+def bpm_test_runner(path):
+    dir = os.listdir(path)
+
+    for filename in dir:
+        wav = path + filename
+        tempo_list = test_aubio(wav)
+        tempo = int(tempo_list[0][0])
+        if tempo > 160 : 
+            tempo = tempo / 2
+        elif tempo < 70 : 
+            tempo = tempo * 2
+        print(filename, str(":"), str(tempo))
+    return
+
 
 test_aubio()
