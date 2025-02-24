e2bd83b240128f6f05f71f5af22e66fcf394d7c1
Henry Clay <mr.henry.clay@gmail.com>
2025-02-24 13:01:25 -0500

    aubio test

--- xf_aubio_implementation_test.py
+++ xf_aubio_implementation_test.py
@@ -8,12 +8,55 @@
 
 import pytest
 import xfader 
-import numpy
-import aubio
 import os
 from pydub import AudioSegment 
 from pydub.playback import play
 
+from aubio import source, tempo
+from numpy import median, diff
+
+#this is basically out of the examples from the docs
+def get_file_bpm(path):
+    """ Calculate the beats per minute (bpm) of a given file.
+        path: path to the file
+        param: dictionary of parameters
+    """
+
+    # default:
+    samplerate, win_s, hop_s = 44100, 1024, 512
+
+    s = aubio.source(path, samplerate, hop_s)
+    samplerate = s.samplerate
+    o = aubio.tempo("specdiff", win_s, hop_s, samplerate)
+    # List of beats, in samples
+    beats = []
+    # Total number of frames read
+    total_frames = 0
+
+    while True:
+        samples, read = s()
+        is_beat = o(samples)
+        if is_beat:
+            this_beat = o.get_last_s()
+            beats.append(this_beat)
+            #if o.get_confidence() > .2 and len(beats) > 2.:
+            #    break
+        total_frames += read
+        if read < hop_s:
+            break
+
+    def beats_to_bpm(beats, path):
+        # if enough beats are found, convert to periods then to bpm
+        if len(beats) > 1:
+            if len(beats) < 4:
+                print("few beats found in {:s}".format(path))
+            bpms = 60./diff(beats)
+            return median(bpms)
+        else:
+            print("not enough beats found in {:s}".format(path))
+            return 0
+
+    return beats_to_bpm(beats, path)
 
 
 def test_aubio():
@@ -24,13 +67,13 @@ def bpm_test_runner(path):
 
     for filename in dir:
         wav = path + filename
-        tempo_list = test_aubio(wav)
-        tempo = int(tempo_list[0][0])
-        if tempo > 160 : 
-            tempo = tempo / 2
-        elif tempo < 70 : 
-            tempo = tempo * 2
-        print(filename, str(":"), str(tempo))
+        tempo_int = get_file_bpm(wav)
+        #bpm = int(tempo_list[0][0])
+        if tempo_int > 160 : 
+            tempo_int = tempo_int / 2
+        elif tempo_int < 70 : 
+            tempo_int = tempo_int * 2
+        print(filename, str(":"), str(tempo_int))
     return
 
 
